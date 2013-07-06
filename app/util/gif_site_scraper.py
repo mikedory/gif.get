79aa1ad60ae059f4bb4561c5dd71883a9052ab4c
Mike Dory <mdory@kbsp.com>
2013-07-06 10:50:35 -0400

    adding a function to save each piece of gif as we go

--- app/util/gif_site_scraper.py
+++ app/util/gif_site_scraper.py
@@ -5,6 +5,8 @@ from tornado.options import define
 import requests
 from bs4 import BeautifulSoup
 
+from models import Gif
+
 # parse the command-line options
 define("gif_site_url", help="the url of the site to scrape", type=str)
 define("gif_site_name", help="the name of the site being scraped", type=str)
@@ -44,6 +46,25 @@ def get_gifs_by_element(element, gif_site_url, gif_site_name):
             print host_name
             print host_url
             print '---'
+
+
+# write in all the newfound gifs
+def update_gif_by_slug(title, slug, img_url, img_type, host_name, host_url):
+
+    # fetch the gif if it already exists
+    gif = Gif.objects.get_or_create(slug=slug)
+
+    # save the parts of the gif
+    gif.title = title
+    gif.slug = slug
+    gif.img_url = img_url
+    gif.img_type = img_type
+    gif.host_name = host_name
+    gif.host_url = host_url
+
+    # save it in
+    result = gif.save()
+    return result
 
 
 if __name__ == "__main__":
