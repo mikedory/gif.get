8e916a0b26cb0ec54918dcfb6930c017f1ad082a
Mike Dory <mdory@kbsp.com>
2013-07-05 20:44:18 -0400

    adding in the handlers for the gifs

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
-#    Sample main.py Tornado file
-#    (for Tornado on Heroku)
-#
-#    Author: Mike Dory | dory.me
-#    Created: 11.12.11 | Updated: 06.02.13
-#    Contributions by Tedb0t, gregory80
-#
-# ------------------------------------------
-
 #!/usr/bin/env python
+
+# import standard things
 import os.path
+import json
+import random
+
+# import tornado things
 import tornado.escape
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 
+# import mongo things
+from mongoengine import *
+
+# import our models
+from models import Gifsite
+from models import Gif
+
 # import and define tornado-y things
 from tornado.options import define
 define("port", default=5000, help="run on the given port", type=int)
+define("mongo_url", default="localhost", help="location of mongodb", type=str)
+define("mongo_port", default=27017, help="port mongodb is listening on", type=int)
+define("mongo_dbname", default="gif-dot-get", help="name of the database", type=str)
 
 
 # application settings and handle mapping info
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/([^/]+)?", MainHandler)
+            (r"/([^/]+)?", IndexHandler),
+            (r"/gif/([^/]+)?", GifHandler),
+            (r"/gifsite/([^/]+)?", GifsiteHandler)
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             debug=True,
         )
+
+        # establish a db connection
+        self.db = connect(
+            tornado.options.options.mongo_dbname,
+            host=tornado.options.options.mongo_url,
+            port=tornado.options.options.mongo_port
+        )
+
+        # define the application
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
 # the main page
-class MainHandler(tornado.web.RequestHandler):
-    def get(self, q):
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, q=None):
         if 'GOOGLEANALYTICSID' in os.environ:
             google_analytics_id = os.environ['GOOGLEANALYTICSID']
         else:
@@ -44,10 +62,53 @@ class MainHandler(tornado.web.RequestHandler):
 
         self.render(
             "main.html",
-            page_title='Heroku Funtimes',
-            page_heading='Hi!',
+            page_title='gif dot get',
+            page_heading='gif.get',
+            page_footer='by @mike_dory',
             google_analytics_id=google_analytics_id,
         )
+
+
+# the main page
+class GifHandler(tornado.web.RequestHandler):
+    def get(self, slug):
+
+        # if there's a gif requested, look it up
+        if slug is not None:
+            try:
+                gif = Gif.objects.get(slug=slug)
+            except DoesNotExist:
+                gif = None
+
+        # if none were requested, return one at random
+        else:
+            # I should probably do this method here instead:
+            # http://stackoverflow.com/questions/2824157/random-record-from-mongodb
+            # but for a tiny set it's fine for now
+            gifs = Gif.objects.all()
+            gif = random.choice(gifs)
+
+        # if that produced a result, return it
+        if gif is not None:
+            self.set_header('Content-Type', 'application/javascript')
+            response = {
+                # "_id": gif["id"],
+                "title": gif["title"],
+                "slug": gif["slug"],
+                # "created_at": gif["created_at"]
+            }
+
+        # if that search came up empty, return a 404
+        else:
+            self.set_status(404)
+            self.set_header('Content-Type', 'application/javascript')
+            response = {
+                "title": "404'd!",
+                "status": "404",
+            }
+
+        # write it out
+        self.write(json.dumps(response))
 
 
 # RAMMING SPEEEEEEED!
