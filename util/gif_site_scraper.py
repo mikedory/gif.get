d8da721a339d3a0161555f86bf18ec1dc5b9a7b5
Mike Dory <mdory@kbsp.com>
2013-07-06 11:20:34 -0400

    updating the scraper to do greatly superior debugging. also: work.

--- util/gif_site_scraper.py
+++ util/gif_site_scraper.py
@@ -1,16 +1,39 @@
+# import stdlib stuff
+import sys
 import os
+import os.path
 
+# import our add-ons
 import tornado
 from tornado.options import define
 import requests
+from mongoengine import *
 from bs4 import BeautifulSoup
 
-from models import Gif
+# import other packages in other directories
+# i am fairly sure this is a bad idea, but it works for now
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+# import our models
+from app.models import Gif
 
 # parse the command-line options
 define("gif_site_url", help="the url of the site to scrape", type=str)
 define("gif_site_name", help="the name of the site being scraped", type=str)
 define("element", default="a", help="the type of tag to search for", type=str)
+define("mongo_url", default="localhost", help="location of mongodb", type=str)
+define("mongo_port", default=27017, help="port mongodb is listening on", type=int)
+define("mongo_dbname", default="gif-dot-get", help="name of the database", type=str)
+
+
+def get_db_connection():
+    db = connect(
+        tornado.options.options.mongo_dbname,
+        host=tornado.options.options.mongo_url,
+        port=tornado.options.options.mongo_port
+    )
+
+    return db
 
 
 # the soup of gifs
@@ -47,29 +70,34 @@ def get_gifs_by_element(element, gif_site_url, gif_site_name):
             print host_url
             print '---'
 
+            # in which gifs are found or created
+            upsert = update_gif_by_slug(title, slug, img_url, img_type, host_name, host_url)
+
+            print upsert
+
 
 # write in all the newfound gifs
 def update_gif_by_slug(title, slug, img_url, img_type, host_name, host_url):
 
-    # fetch the gif if it already exists
-    gif = Gif.objects.get_or_create(slug=slug)
+    # fetch the gif if it already exists, create it if it doesn't
+    gif, created = Gif.objects.get_or_create(
+        slug=slug,
+        title=title,
+        img_url=img_url,
+        img_type=img_type,
+        host_name=host_name,
+        host_url=host_url
+    )
 
-    # save the parts of the gif
-    gif.title = title
-    gif.slug = slug
-    gif.img_url = img_url
-    gif.img_type = img_type
-    gif.host_name = host_name
-    gif.host_url = host_url
-
-    # save it in
-    result = gif.save()
-    return result
+    # return the gif and the true/false of its creation
+    return gif, created
 
 
+# let's do this
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = get_gifs_by_element(
+    get_db_connection()
+    get_gifs_by_element(
         tornado.options.options.element,
         tornado.options.options.gif_site_url,
         tornado.options.options.gif_site_name)
