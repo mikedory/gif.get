ed0ff0bb254ad78910ace3e7369042f54da762f7
Mike Dory <mdory@kbsp.com>
2013-07-09 11:16:32 -0400

    just kidding, this should totally be part of the model

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ class Application(tornado.web.Application):
 
 
 # format gifs for json output
-def format_gif_for_json_response(gif, href):
+def format_gif_for_json_response(gif):
     single_response = {
         "title": gif["title"],
         "slug": gif["slug"],
@@ -67,21 +67,21 @@ def format_gif_for_json_response(gif, href):
         "host_name": gif["host_name"],
         "host_url": gif["host_url"],
         "tags": gif["tags"],
-        "href": href,
+        "href": gif["href"],
         "created_at": str(gif["created_at"])
     }
     return single_response
 
 
 # format gifsites for json output
-def format_gifsite_for_json_response(gifsite, href):
+def format_gifsite_for_json_response(gifsite):
     single_response = {
         "title": gifsite["title"],
         "slug": gifsite["slug"],
         "url": gifsite["url"],
         "description": gifsite["description"],
         "tags": gifsite["tags"],
-        "href": href,
+        "href": gifsite["href"],
         "created_at": str(gifsite["created_at"])
     }
     return single_response
